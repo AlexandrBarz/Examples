642a242c457ed27e02f63aeaa20be13ac5d6f743
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 09:48:01 +0300

    added  Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine(); // = оператор присваивания
+
+if (username.ToLower() == "маша") // == оператор сравнения; ToLower переводит все символы в нижней регистр, при чем выражение в " " д.б. записано нижним регистром
+{
+   Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
