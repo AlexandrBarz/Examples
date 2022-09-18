9b9efda0f9ad8378ffeb1dc71d84e505bec5a9e6
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 10:19:13 +0300

    corrected Example002

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введитеваше имя:");
+﻿Console.WriteLine("Введите ваше имя:");
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
