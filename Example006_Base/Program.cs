75918de688d57b85f3df554c3e0dec037ae42093
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 09:57:27 +0300

    added  Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 6;
+int c = 8;
+int d = 5;
+int e = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
